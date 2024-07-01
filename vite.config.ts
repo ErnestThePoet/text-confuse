5c98e2871531bb3ba630cabae4dadb72c7856855
Ernest Cui <ecuiships@126.com>
2024-07-01 22:31:13 +0800

    build: modify info

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/text-confuse",
+  build: {
+    outDir: "./docs",
+  },
+  server: {
+    host: true,
+  },
   plugins: [react()],
-})
+});
